a68780652827d40914a187692735fc1efc994de8
pavangbhat <pavanbhat17@gmail.com>
2021-10-12 09:20:56 +0530

    Initial version of dark-mode

--- components/Goals.vue
+++ components/Goals.vue
@@ -72,7 +72,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #f8f8f8;
+  background-color: var(--background-card);
   margin: 10px 0px;
   padding: 25px 30px;
 }
@@ -94,7 +94,7 @@ export default {
 .goals-task label {
   width: 70px;
   height: 70px;
-  background: #c4c4c4;
+  background: var(--background-primary);
   transition: 400ms 100ms ease-out;
   cursor: pointer;
 }
@@ -157,7 +157,7 @@ export default {
   }
 }
 .goals-task h1 {
-  color: rgba(6, 6, 6, 0.75);
+  color: var(--color-primary);
   font-family: 'Roboto', sans-serif;
   font-size: 36px;
   font-weight: 700;
